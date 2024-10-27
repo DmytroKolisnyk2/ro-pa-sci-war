6145d5437a793aefc314667f8e790ca3d4a8b0d1
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-10-27 20:59:06 +0200

    feat: added prisma and basic schema

---
+++ prisma/migrations/20241027182816_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdRoomId" INTEGER,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_createdRoomId_fkey" FOREIGN KEY ("createdRoomId") REFERENCES "Room"("id") ON DELETE SET NULL ON UPDATE CASCADE;
